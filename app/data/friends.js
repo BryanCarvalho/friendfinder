49ac44eb921338a586e3cdb401040d138f9fa50a
Bryan C <bryan.carvalho.design@gmail.com>
2018-11-04 15:18:56 -0500

    Add friend objects

---
+++ app/data/friends.js
@@ -0,0 +1,28 @@
+
+let friends = [
+    {
+        "name": "Ahmed",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores": [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+    },
+    {
+        "name": "Vince",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores": [3, 2, 6, 4, 5, 1, 2, 5, 4, 1]
+    },
+    {
+        "name": "Troy",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores": [2, 5, 4, 2, 5, 3, 4, 2, 3, 1]
+    },
+    {
+        "name": "Jasmine",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores": [1, 1, 3, 1, 1, 1, 5, 1, 1, 2]
+    },
+    {
+        "name": "Elizabeth",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores": [4, 3, 2, 5, 3, 2, 1, 4, 2, 3]
+    },
+] 
