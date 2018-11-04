795476e6fd75a90693bcd84b04f164047c505054
Bryan C <bryan.carvalho.design@gmail.com>
2018-11-04 15:56:06 -0500

    Added HTML

--- app/data/friends.js
+++ app/data/friends.js
@@ -25,4 +25,7 @@ let friends = [
         "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
         "scores": [4, 3, 2, 5, 3, 2, 1, 4, 2, 3]
     },
-] 
+]
+
+module.exports = friends;
+
